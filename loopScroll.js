a5eca4f3e5a2729eab2d6a632363dfd596610ecd
root <root@localhost.localdomain>
2012-06-29 16:42:54 -0400

    代码整理

--- loopScroll.js
+++ loopScroll.js
@@ -1,114 +1,127 @@
 (function($){
-	var loopScroll = function(content, options){
-		var $u = $('ul',content);
-			$u.append($u.find('li:first').clone())
-			$u.append($u.find('li:eq(1)').clone())
-			$u.prepend($u.find('li:eq(1)').clone())
-		var $l = $('li',$u).css('float','left');
-		var params = {
-			'$u'     : $u,
-			'$l'     : $l,
-			'len'    : $l.length,
-			'loaded' : true,
-			'i'      : 1,
-			'$i'     : $(options.index, content)
-		}
-		this.options = $.extend({}, params, options);
- 		this.init()
-	}
+var loopScroll = function(content, options){
+    var $u = $('ul',content),
+        $first = $u.find('li:first').clone(),
+        $last = $u.find('li:last').clone(),
+        $second = $u.find('li:eq(1)').clone();
 
-	loopScroll.prototype = {
-		init : function(){
-			var options = this.options, me = this;
-			options.$l.find('img').load(function(){
-				if(options.loaded){
-					options.w = w = options.$l.width();
-					options.$u.width( w * options.len ).css( 'left', -w );
-					options.$l.css('opacity',0.5).eq(1).css('opacity',1);
-					me.run();
-					options.loaded = false;
-				}
-			})
-			this.bind()
-		},
+    $u.append($first)
+    $u.prepend($last)
+    $u.append($second)
 
-		run : function(){
-			var options = this.options, me = this;
-			options.cle && clearInterval(options.cle);
-			options.cle = setInterval(function(){
-							options.i++		
-							if( options.i >= options.len-1){
-								options.i = 1
-								options.$u.css('left',-options.w)
-								arguments.callee()
-							}else{
-								scrollImg.call( options )
-							}
-							
-						},options.speed);
-		},
+    var $l = $('li',$u).css('float','left');
+    var params = {
+        '$u'     : $u,
+        '$l'     : $l,
+        'len'    : $l.length,
+        'loaded' : true,
+        'i'      : 1,
+        '$i'     : $(options.index, content)
+    }
+    this.options = $.extend({}, params, options);
+    this.init()
+}
 
-		bind : function(){
-			var options = this.options, me = this;
-			if( options.mouseWheel ){
-				options.$u.mouseover( function(){ clearInterval(options.cle) } )
-						  .mouseout( function(){ me.run() } )
-			}
-			$(options.btnNext).click($.proxy( nextHandle, options ))
-			$(options.btnPrev).click($.proxy( prevHandle, options ))
-			$(options.btnNext).mouseout( function(){ me.run() } )
-			$(options.btnPrev).mouseout( function(){ me.run() } )
-		}
-	}
+loopScroll.prototype = {
+    init : function(){
+            var options = this.options, me = this;
+            options.$l.find('img').load(function(){
+                    if(options.loaded){
+                    var w = options.w = options.$l.width();
+                    options.$u.width( w * options.len ).css( 'left', -w );
+                    options.$l.css('opacity',0.5).eq(1).css('opacity',1);
+                    me.run();
+                    options.loaded = false;
+                    }
+                    })
+            this.bind()
+        },
 
-	var nextHandle = function(){
-		clearInterval(this.cle);
-		this.$u.stop(true, true)
-		if(this.i == (this.len-2)){
-			this.i = 1;
-			this.$u.css('left',-this.w);
-		} 
-		this.i++;
-		scrollImg.call( this )
-	}
+    run : function(){
+            var options = this.options, me = this;
+            options.cle && clearInterval(options.cle);
+            options.cle = setInterval(function(){
+                    options.i++
+                    if( options.i >= options.len-1){
+                    options.i = 1
+                    options.$u.css('left',-options.w)
+                    arguments.callee()
+                    }else{
+                    scrollImg.call( options )
+                    }
 
-	var prevHandle = function(){
-		clearInterval(this.cle);
-		this.$u.stop(true, true)
-		if( this.i == 1 ){
-			this.i = this.len-1;
-			this.$u.css('left',-this.w*(this.len-1))
-		}
-		this.i--;
-		scrollImg.call( this )
-	}
+                    },options.speed);
+        },
 
-	var scrollImg = function(){
-		this.$u.stop(true, true).animate( { 'left': -this.w*this.i },350 );
-		this.$i.find('a').eq(this.i-1).addClass( this.index_class )
-							     .siblings().removeClass( this.index_class );
-							     
-		this.$l.css('opacity',0.5).eq(this.i).css('opacity',1);
+    bind : function(){
+            var options = this.options, me = this;
+            if( options.mouseWheel ){
+                options.$u.mouseover( function(){ clearInterval(options.cle) } )
+                    .mouseout( function(){ me.run() } )
+            }
+            $(options.btnNext).click($.proxy( nextHandle, options ))
+                $(options.btnPrev).click($.proxy( prevHandle, options ))
+                //$(options.btnNext).mouseout( function(){ me.run() } )
+                //$(options.btnPrev).mouseout( function(){ me.run() } )
+        }
+}
 
-		this.i == this.len-1 && $('a:eq(0)',this.$i).addClass( this.index_class ).siblings().removeClass( this.index_class );
-	}
+ var nextHandle = function(){
+     clearInterval(this.cle) 
+         this.$u.stop(true, false)
+         if(this.i == (this.len-2)){
+             this.i = 1;
+             this.$u.css('left',-this.w);
+         } 
+     //console.log(new Date().getSeconds())
+     this.i++;
+     scrollImg.call( this )
+         return false;
+ }
 
-	$.fn.loopScroll = function(option){
-		 var defaults = {
-				btnNext     : ".next_btn",
-				btnPrev     : ".pre_btn",
-				index       : ".img_count",
-				index_class : 'active',
-				mouseWheel  : true,
-				speed       : 4000
-		  }
+ var prevHandle = function(){
+     clearInterval(this.cle)
+         this.$u.stop(true, false)
 
-		return this.each(function(){
-				var $this    = $(this)
-		        	,data    = $this.data('loopScroll')
-		       		,options = $.extend({}, defaults, $this.data(), typeof option == 'object' && option)
-		      if (!data) $this.data('loopScroll', (data = new loopScroll(this, options)))
-		})
-	}
+         if( this.i == 1 ){
+             this.i = this.len-2;
+             this.$u.css('left',-this.w*(this.i))
+         }
+
+     this.i--;
+     scrollImg.call( this )
+         return false;
+ }
+
+ var scrollImg = function(){
+     this.$l.eq(this.i-1).animate({'opacity':0.5},500)
+         this.$l.eq(this.i).animate({'opacity':1},500);
+     this.$u.stop(true, true).animate( { 'left': -this.w*this.i }, 800,function(){
+
+             });
+     this.$i.find('a').eq(this.i-1).addClass( this.index_class )
+         .siblings().removeClass( this.index_class );
+
+
+     //this.i == this.len-1 && $('a:eq(0)',this.$i).addClass( this.index_class ).siblings().removeClass( this.index_class );
+ }
+
+ $.fn.loopScroll = function(option){
+     var defaults = {
+              btnNext     : ".next_btn",
+              btnPrev     : ".pre_btn",
+              index       : ".img_count",
+              index_class : 'active',
+              mouseWheel  : true,
+              speed       : 4000
+     }
+
+     return this.each(function(){
+             var $this    = $(this)
+             ,data    = $this.data('loopScroll')
+             ,options = $.extend({}, defaults, $this.data(), typeof option == 'object' && option)
+             if (!data) $this.data('loopScroll', (data = new loopScroll(this, options)))
+             })
+ }
 
 })(jQuery)
